68fdf8a9c5ba15854ed92762a24415e9fdd08cbd
John Ostrander <john@crackroot.org>
2017-06-24 17:15:04 -0400

    notes

--- TouchMyCAC.sh
+++ TouchMyCAC.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-#Test comment please ignore
+
+#######################################################################################################
+# Thank the flying toasters for the Digital Ocean wiki and stack exchange
+
+#######################################################################################################
+# Generate new random 16 char passwords
 NewRootPassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 NewUserPassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
+#######################################################################################################
+# Get some variables from the user
 read -r -p "What is the CAC IP:       #: " CACIP;
 while [[ ${CACIPSet} != "y" ]]; do
         if [[ ${CACIP} == "" ]]; then
@@ -39,73 +46,103 @@ while [[ ${NewUserNameSet} != "y" ]]; do
         fi
 done
 
+#######################################################################################################
+# Generate new SSH key pair
 ssh-keygen -b 4096 -t rsa -f CACid_rsa -q -N "";
 if [ ! -d "~/.ssh" ]; then
 	mkdir ~/.ssh
 fi
-cp CACid_rs* ~/.ssh/
+cp CACid_rsa* ~/.ssh/
 
+#######################################################################################################
+# Log into server and install SSH pub key. This is kinda hacky, and might need some work
 echo ""
 expect -c "  
-	set timeout 1
-	spawn ssh root@${CACIP} \'mkdir .ssh\'
+	set timeout 9
+	spawn ssh root@${CACIP} mkdir /root/.ssh/
+	sleep 3
 	expect \(yes\/no\)\? { send yes\r }
+	sleep 3
 	expect password: { send ${DefaultRootPassword}\r }
-	sleep 1
+	sleep 3
 	exit
 "
-sleep 2
 
 echo ""
 expect -c "  
-	set timeout 1
-	spawn scp CACid_rsa.pub root@${CACIP}:.ssh/authorized_keys
+	set timeout 6
+	spawn scp CACid_rsa.pub root@${CACIP}:/root/.ssh/authorized_keys
+	sleep 3
 	expect password: { send ${DefaultRootPassword}\r }
 	expect 100%
-	sleep 1
+	sleep 3
 	exit
 "
-echo ""
 
-ssh-add CACid_rsa
-chmod 600 CACid_rsa*
-ssh -i CACid_rsa root@${CACIP} useradd -m ${NewUserName}
-ssh -i CACid_rsa root@${CACIP} "echo ${NewUserName}:${NewUserPassword} | chpasswd"
-ssh -i CACid_rsa root@${CACIP} usermod -a -G sudo ${NewUserName}
-ssh -i CACid_rsa root@${CACIP} wget http://download.opensuse.org/repositories/network:bro/xUbuntu_14.04/Release.key
-ssh -i CACid_rsa root@${CACIP} apt-key add Release.key
-ssh -i CACid_rsa root@${CACIP} rm Release.key
-ssh -i CACid_rsa root@${CACIP} apt update
-ssh -i CACid_rsa root@${CACIP} apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
-ssh -i CACid_rsa root@${CACIP} apt -y install bro fail2ban firefox iptables-persistent synaptic tcpdump vim wireshark xfce4 xfce4-dict xfce4-goodies xfce4-terminal xubuntu-icon-theme
-ssh -i CACid_rsa root@${CACIP} service fail2ban stop
-ssh -i CACid_rsa root@${CACIP} awk '{ printf "# "; print; }' /etc/fail2ban/jail.conf | tee /etc/fail2ban/jail.local
-ssh -i CACid_rsa root@${CACIP} iptables -F
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -i lo -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p tcp --dport 4000 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p upd --dport 22 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p udp --dport 80 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p udp --dport 443 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p udp --dport 4000 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
-ssh -i CACid_rsa root@${CACIP} iptables -A INPUT -j DROP
-ssh -i CACid_rsa root@${CACIP} dpkg-reconfigure iptables-persistent
-ssh -i CACid_rsa root@${CACIP} service fail2ban start
-scp -i CACid_rsa NoMachine.deb root@${CACIP}:
-ssh -i CACid_rsa root@${CACIP} dpkg -i NoMachine.deb
-ssh -i CACid_rsa root@${CACIP} rm NoMachine.deb
-ssh -i CACid_rsa root@${CACIP} /opt/bro/bin/broctl install
-ssh -i CACid_rsa root@${CACIP} /opt/bro/bin/broctl start
-ssh -i CACid_rsa root@${CACIP} echo "#!/usr/bin/env sh" >> /etc/init.d/S97-setup.sh
-ssh -i CACid_rsa root@${CACIP} echo "/opt/bro/bin/broctl start" >> /etc/init.d/S97-setup.sh
-ssh -i CACid_rsa root@${CACIP} reboot
+#######################################################################################################
+# Install new SSH key pair on local box
+ssh-add ~/.ssh/CACid_rsa
 
+#######################################################################################################
+# Create new users on CAC and add them to sudo, also change root password
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} useradd -m ${NewUserName}
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} chsh -s /bin/bash ${NewUserName}
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} "echo ${NewUserName}:${NewUserPassword} | chpasswd"
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} usermod -a -G sudo ${NewUserName}
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} "echo root:${NewRootPassword} | chpasswd"
+
+#######################################################################################################
+# Add repositories, run updates, and install junk
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} wget http://download.opensuse.org/repositories/network:bro/xUbuntu_14.04/Release.key
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} apt-key add Release.key
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} rm Release.key
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} apt update
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} apt -y install bro fail2ban firefox iptables-persistent synaptic tcpdump vim wireshark xfce4 xfce4-dict xfce4-goodies xfce4-terminal xubuntu-icon-theme
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} "awk \'{ printf \"# \"; print; }\' /etc/fail2ban/jail.conf | tee /etc/fail2ban/jail.local"
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} service fail2ban stop
+
+#######################################################################################################
+# Firewall all the things
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -F
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -i lo -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p tcp --dport 4000 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p upd --dport 22 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p udp --dport 80 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p udp --dport 443 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p udp --dport 4000 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} iptables -A INPUT -j DROP
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} dpkg-reconfigure iptables-persistent
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} service fail2ban restart
+
+#######################################################################################################
+# Get NoMachine installed for easy peasy remote desktop
+scp -i ~/.ssh/CACid_rsa NoMachine.deb root@${CACIP}:
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} dpkg -i NoMachine.deb
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} rm NoMachine.deb
+
+#######################################################################################################
+# Get Bro IDS installed because I'm a bro bro and bro is the way to be cool
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} /opt/bro/bin/broctl install
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} /opt/bro/bin/broctl start
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} echo "#!/usr/bin/env sh" >> /etc/init.d/S97-setup.sh
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} echo "/opt/bro/bin/broctl start" >> /etc/init.d/S97-setup.sh
+ssh -i ~/.ssh/CACid_rsa root@${CACIP} reboot
+
+#######################################################################################################
+# rm SSH keypair from pwd, and install an alias for easy sshing
 rm CACid_rsa*
+touch ~/.bash_aliases
+echo -e "alias ssh-cac=\'ssh -i ~/.ssh/CACid_rsa root@${CACIP}\'" >> ~/.bash_aliases
 
+#######################################################################################################
+# Tell user that they need to use the passwords we generated up top
 echo -e "\nMake sure you record these new passwords somewhere or you will be sad."
+echo -e "\n${CACIP}"
 echo -e "\nroot password: ${NewRootPassword}"
 echo -e "\n${NewUserName} password: ${NewUserPassword}"
